c0f99110b816241e1467c2dd48af1b653d618056
antONget <64537429+antONget@users.noreply.github.com>
2023-12-16 17:18:02 +0300

    Update config.py
    admin menu

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 token_bot = "6469133428:AAGNVYWtBhl8HZrFv0t4hx1dXCs0t693t1Q"
-# token_bot = "6466209926:AAGVykzUlt4tQm6yY8P9rjTp4S8h882_sxk"
-email ="stavros-service@aquarel-406814.iam.gserviceaccount.com"
+# token_bot = "6689173506:AAGWKlb1YFtiWdKR0qh_dTFLwxax7SIVG8s"  # test
+email = "stavros-service@aquarel-406814.iam.gserviceaccount.com"
+admin_ids = [272628852, 843554518]
